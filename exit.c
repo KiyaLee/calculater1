b3dbbd406c7dc334df1ccad471b569d94b8e15a6
kdak489 <kdak489@gmail.com>
2016-05-30 20:52:20 +0900

    Update exit.c

--- exit.c
+++ exit.c
@@ -1,8 +1,8 @@
 #include <stdlib.h>
 
-void display_clear(char dis[10]);
+void display_exit(char dis[10]);
 
-void display_clear(char dis[10])
+void display_exit(char dis[10])
 {
   scanf("%s", &dis[10]);
   if(dis=='end')
