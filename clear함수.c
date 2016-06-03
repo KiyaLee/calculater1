b27e75e554dd0820fd5d3050537c840aba4b0c14
tim2875 <tim2875@naver.com>
2016-06-03 19:14:44 +0900

    Add files via upload
    조건은 주석문으로

---
+++ clear함수.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+void Clear(char dis[10])
+{	//if (dis[0] == 'c' && dis[1] == 'l' && dis[2] == 'e' && dis[3] == 'a' && dis[4] == 'r') - 조건
+		system("clear");
+}
---
+++ end함수.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+void End(char dis[10]) 
+{ 
+	//if (dis[0] == 'e' && dis[1] == 'n' && dis[2] == 'd') - 조건
+	    exit(-1); 
+} 
