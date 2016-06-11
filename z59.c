44399d1aed492a405813eab640d1dfb14f10a550
DaBinz <hhbsss@naver.com>
2016-06-12 00:14:38 +0900

    Add files via upload

---
+++ z59.c
@@ -0,0 +1,17 @@
+int i = 0, count;  
+char x = '\0';
+   
+while(fir[i] != '.'){
+     i++;
+         }
+count = i;
+if(i > 50){
+  return x;
+         }
+while(fir[i] != '\0'){
+     i++;
+     }
+if((i-count)>10){
+  return x;
+    }
+
